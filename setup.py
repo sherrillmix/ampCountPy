ad88c8d2e2ca594e2df071d0a64dd9d69f51ddcd
sherrillmix <sherrillmix@users.noreply.github.com>
2015-10-04 07:57:18 -0400

    set up setup.py to run test

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 from setuptools import setup, find_packages
+import sys
+from setuptools.command.test import test as TestCommand
+
+#https://pytest.org/latest/goodpractises.html
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(name='ampcountpy',
     version='0.1',
@@ -8,5 +29,7 @@ setup(name='ampcountpy',
     author_email='shescott@upenn.edu',
     license='GPL-2',
     packages=find_packages(),
-    zip_safe=True
+    zip_safe=True,
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
 )
